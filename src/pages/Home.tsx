51316b5c2b7b2e18fabebdd8ed4e1ab3089eeb24
souveniersth-creator <souveniersth@gmail.com>
2026-04-08 15:41:47 +0545

    video added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,9 +26,9 @@ export default function Component() {
             <span className="text-[20vw] font-headline font-bold text-on-surface leading-none">RAWCINE</span>
           </div>
 
-          <div className="relative w-full max-w-6xl h-[614px] md:h-[716px] flex items-center justify-center overflow-hidden">
+          <div className="relative w-full max-w-5xl aspect-video flex items-center justify-center overflow-hidden mx-auto mt-8">
             <iframe
-              className="absolute top-1/2 left-1/2 w-auto max-w-none h-[120%] md:h-[105%] aspect-[16/9] -translate-x-1/2 -translate-y-1/2 pointer-events-none"
+              className="absolute top-1/2 left-1/2 w-[110%] h-[110%] -translate-x-1/2 -translate-y-1/2 pointer-events-none"
               src="https://www.youtube.com/embed/hkjtFKvHRT0?autoplay=1&mute=1&controls=0&loop=1&playlist=hkjtFKvHRT0&rel=0&modestbranding=1&playsinline=1&vq=hd1080"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
